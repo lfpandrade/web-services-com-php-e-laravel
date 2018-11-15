f3346a0759783da56aef4114d2cc2bacae195de6
Luis Fernando <lfpandrade@bol.com.br>
2018-11-15 09:39:23 -0200

    Alteração do method get no search

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ $this->group(['prefix' => 'v1'], function() {
 
     //AUTENTICANTO COM O MIDDLEWARE JWT.AUTH
     $this->group(['middleware' => 'jwt.auth'], function() {
-        $this->post('products/search', 'Api\V1\ProductController@search');
+        $this->get('products/search', 'Api\V1\ProductController@search');
         $this->resource('products', 'Api\V1\ProductController', ['except' => ['create', 'edit']]);
     });
 });
